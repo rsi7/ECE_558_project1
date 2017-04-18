56e8bd9f456b5d88bd424b2a474c48bf0cbd8890
Rehan Iqbal <rsi7.unc@gmail.com>
2017-04-17 20:26:10 -0700

    Added commenting and exception handling

--- java/Quiz.java
+++ java/Quiz.java
@@ -1,31 +1,55 @@
 /**
  * @author Rehan Iqbal(riqbal@pdx.edu) <BR> <BR>
  *
- *
+ * This module handles the overall Quiz functionality, which is nothing
+ * more than a collection of five questions (QuizItems) and a few methods
+ * for executing them in sequence and printing results. There is also a test
+ * client under 'main' if you need an example of how to run it.
  */
 
 import java.util.Formatter;
+import java.util.Collections;
+import java.util.Arrays;
 
 public class Quiz {
 
     private QuizItem[]  QuizItemArray;
 
-    private String[][]  QuizChoices = { {"A: Chien", "B: Perro", "C: Inu", "D: Hund"},
-                                        {"A: Gato", "B: Chat", "C: Neko", "D: Catze"} };
-
     private int         NumCorrectAnswers = 0;
     private double      PercentCorrect = 0.0;
 
+    /**
+     * Default constructor.  Initializes five QuizItems with their questions,
+     * choices, and correct answer.
+     */
+
     public Quiz() {
 
-        QuizItemArray = new QuizItem[2];
+        String[][]  QuizChoices = { {"A: Chien", "B: Pero", "C: Inu", "D: Hund"},
+                                    {"A: Chat", "B: Gato", "C: Neko", "D: Catze"},
+                                    {"A: Poisson", "B: Sakana", "C: Fisch", "D: Peces"},
+                                    {"A: Conejo", "B: Hase", "C: Lapin", "D: Usagi"},
+                                    {"A: Vogel", "B: Pajaro", "C: Oiseau", "D: Tori"} };
+
+        QuizItemArray = new QuizItem[5];
 
         QuizItemArray[0] = new QuizItem("What is the Japanese word for dog?", QuizChoices[0], 'C');
-        QuizItemArray[1] = new QuizItem("What is the Spanish word for cat?", QuizChoices[1], 'A');
+        QuizItemArray[1] = new QuizItem("What is the Spanish word for cat?", QuizChoices[1], 'B');
+        QuizItemArray[2] = new QuizItem("What is the French word for fish?", QuizChoices[2], 'A');
+        QuizItemArray[3] = new QuizItem("What is the German word for rabbit?", QuizChoices[3], 'B');
+        QuizItemArray[4] = new QuizItem("What is the Japanese word for bird?", QuizChoices[4], 'D');
 
     }
 
+    /**
+     * Method to execute the quiz - it will run through all questions in a
+     * shuffled, random order. It will also log the number of correct answers
+     * to the Quiz object.
+     */
+
     public void runQuiz() {
+
+        Collections.shuffle(Arrays.asList(QuizItemArray));
 
         for (int i = 0; i < QuizItemArray.length; i++) {
 
@@ -35,11 +59,22 @@ public class Quiz {
         }
     }
 
+    /**
+     * Method to crunch the quiz statistics & print them to console.
+     * It will format print the percentage of correct answers.
+     */
+
     public void printResults() {
 
         PercentCorrect = ( NumCorrectAnswers / ( (double) QuizItemArray.length) ) * 100;
         System.out.format("\nYou got %4.2f%% correct\n", PercentCorrect);
     }
+
+    /**
+     * Test client for the Quiz object, available as a main method.
+     *
+     * @param args - default command line arguments from console (not used by Quiz)
+     */
 
     public static void main ( String args[] ) {
 
--- java/QuizItem.java
+++ java/QuizItem.java
@@ -1,10 +1,14 @@
 /**
  * @author Rehan Iqbal(riqbal@pdx.edu) <BR> <BR>
  *
- *
+ * This module handles the functionality for one multiple-choice question.
+ * It holds the question statement, possible choices, user answer, and
+ * whether they got it right or not. It also has methods to 'ask' itself
+ * to the user, collect the response, and check for correctness.
  */
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class QuizItem {
 
@@ -12,18 +16,43 @@ public class QuizItem {
     private String      QuizItemQuestion;
     private String[]    QuizItemChoices;
 
-    private boolean     CorrectAnswer;
+    private boolean     AnsweredCorrect;
     private char        UserResponse;
+
+    /**
+     * Default constructor.  Checks the number of answer choices
+     * and validity of the answer argument before assigning to object fields.
+     * @param QuizItemQuestion - the question expressed as a sentence or equation
+     * @param QuizItemChoices - an array of possible choices
+     * @param QuizItemAnswer - the correct answer for the QuizItemQuestion
+     */
 
     public QuizItem(String QuizItemQuestion, String[] QuizItemChoices, char QuizItemAnswer) {
 
-        // validate the data for constructor
-
         this.QuizItemQuestion = QuizItemQuestion;
-        this.QuizItemChoices = QuizItemChoices;
-        this.QuizItemAnswer = QuizItemAnswer;
+
+        // validate the multiple-choice options
+
+        if (QuizItemChoices.length >= 4) {
+            this.QuizItemChoices = QuizItemChoices;
+        }
+
+        else throw new Error("\nNeed at least 4 multiple-choice options!");
+
+        // validate the provided answer
+
+        if ( String.valueOf(QuizItemAnswer).matches("[abcdABCD]") ) {
+            this.QuizItemAnswer = QuizItemAnswer;
+        }
+
+        else throw new Error("\nNeed a valid character (A,B,C,D) for answer!");
 
     }
+
+    /**
+     * Method to 'ask' the user this QuizItem by printing it to console.
+     * It will print the question & all possible choices.
+     */
 
     public void askQuestion() {
         System.out.println("\n" + QuizItemQuestion + "\n");
@@ -33,35 +62,57 @@ public class QuizItem {
         }
     }
 
+    /**
+     * Method to ask grab the user's input. It will check for
+     * a valid entry (i.e. a,b,c,d character) and store it
+     * in the object's UserResponse field.
+     */
+
     public void collectResponse() {
 
-        String s;
+        String str;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("\nType a character: ");
 
         while (!scan.hasNext()) {
             String garbage = scan.nextLine();
-            System.out.println("Input failed. Please type a character: ");
+            System.out.println("\nInput failed. Please type a valid character (A,B,C,D): ");
         }
 
-        s = scan.next( );
-        UserResponse = s.toUpperCase().charAt(0);
+        do {
+            str = scan.next();
+            if ( str.matches("[abcdABCD]") ) UserResponse = str.toUpperCase().charAt(0);
+            else System.out.println("\nInput failed. Please type a valid character (A,B,C,D): ");
+
+        } while (UserResponse == '\u0000');
     }
+
+    /**
+     * Method to compare the user's answer against the official answer
+     * (as provided at Quiz creation time).
+     * @return AnsweredCorrect - boolean reflecting whether user got it right or not
+     */
 
     public boolean checkResult() {
 
-        CorrectAnswer = (UserResponse == QuizItemAnswer) ? true : false;
+        AnsweredCorrect = (UserResponse == QuizItemAnswer) ? true : false;
 
-        if (CorrectAnswer) System.out.println("\nCorrect!");
-        else System.out.println("\nWrong - correct answer was: " + String.valueOf(QuizItemAnswer));
+        if (AnsweredCorrect) System.out.println("\nCorrect!");
+        else System.out.println("\nWrong - correct answer was " + String.valueOf(QuizItemAnswer) + ".");
 
-        return CorrectAnswer;
+        return AnsweredCorrect;
     }
+
+    /**
+     * Method to print the question statement - not used by Quiz,
+     * but may be useful for log files.
+     * @return QuizItemQuestion - the question statement given to this QuizItem
+     */
 
     public String toString() {
 
-            return ( QuizItemQuestion + "\n\n" + "Answer: " + String.valueOf(UserResponse) );
+            return QuizItemQuestion;
     }
 
 }
